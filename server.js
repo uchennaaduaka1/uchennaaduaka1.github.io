746da580214a008bf19e6d253798a313cce80ab2
uchennaaduaka1 <55813197+uchennaaduaka1@users.noreply.github.com>
2020-10-02 00:11:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.route('/api')
   })
   .post((req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body)
+    console.log('Form data in res.body', req.body);
+    res.status(200).send(('Hello World'))
   }}
 
 app.listen(port, () => {
