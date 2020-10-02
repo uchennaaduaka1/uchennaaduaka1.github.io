363d596ff43ad69596d21276d8ac5844081ac844
uchennaaduaka1 <55813197+uchennaaduaka1@users.noreply.github.com>
2020-10-02 00:08:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body)
-  });
+  }}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
