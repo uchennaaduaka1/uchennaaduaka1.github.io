4a99acba45c2ae409ef79abf33830ba90a2bd331
Uchenna Aduaka <uchennaaduaka4@gmaill.com>
2020-10-28 19:00:30 -0400

    some of lab 7 done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import countries from './public/lab_6/countries.js';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -26,9 +27,13 @@ app.route('/api')
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
   .post((req, res) => {
-    res.json(countries);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json(json);
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+
+    res.json(countries);
   });
 
 app.listen(port, () => {
